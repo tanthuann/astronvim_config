a32c00ab7a6a4780ef5bb3f3c9dddd579f4a01f9
tanthuann <truongtanthuan99@gmail.com>
2024-05-03 22:25:08 +0700

    First commit

---
+++ lua/community.lua
@@ -0,0 +1,16 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.colorscheme.vscode-nvim"},
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.html-css"},
+  { import = "astrocommunity.pack.sql" },
+  -- import/override with your plugins folder
+}
